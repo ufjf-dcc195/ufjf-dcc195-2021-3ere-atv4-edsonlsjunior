c7bb31f131e7c88e104601378ed935f37bac02a3
edsonlsjunior <edsonjunior@ice.ufjf.br>
2022-01-05 12:38:42 -0300

    Cria middleware em connect

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const connect = require("connect");
 const app = connect();
 app.listen(3000);
+app.use(helloWorld);
+
+function helloWorld(req, res, next){
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Hello, World!</h1>");
+}
 
 console.log("Servidor escutando em http://localhost:3000");
