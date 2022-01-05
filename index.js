ad1a164d620a828254a25d7cb60901816cf0eaeb
edsonlsjunior <edsonjunior@ice.ufjf.br>
2022-01-05 12:43:24 -0300

    Adiciona filter middleware

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const connect = require("connect");
 const app = connect();
 app.listen(3000);
+app.use(logger);
 app.use(helloWorld);
+
+
+console.log("Servidor escutando em http://localhost:3000");
 
 function helloWorld(req, res, next){
     res.setHeader("Content-Type", "text/html");
     res.end("<h1>Hello, World!</h1>");
 }
 
-console.log("Servidor escutando em http://localhost:3000");
+function logger(req, res, next){
+    console.log("Nova requisição!\n");
+
+    next();
+}
