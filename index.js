a66aa28beed158612f20121eae92c8ec932188a1
edsonlsjunior <edsonjunior@ice.ufjf.br>
2022-01-06 18:28:41 -0300

    adicionar segundo modulo

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const connect = require("connect");
 const app = connect();
 app.listen(3000);
 app.use(logger);
-app.use(helloWorld);
+app.use("/hello.html", helloWorld);
+app.use("/bye.html", byeWorld);
 
 
 console.log("Servidor escutando em http://localhost:3000");
@@ -12,8 +13,13 @@ function helloWorld(req, res, next){
     res.end("<h1>Hello, World!</h1>");
 }
 
+function byeWorld(req, res, next){
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Bye, World!</h1>");
+}
+
 function logger(req, res, next){
-    console.log("Nova requisição!\n");
+    console.log("Nova requisição!\n", req.url);
 
     next();
 }
