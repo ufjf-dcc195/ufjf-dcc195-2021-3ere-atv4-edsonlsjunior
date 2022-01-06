383ffdf703b2ca4cd7bc2ce73bb7374015cac21a
edsonlsjunior <edsonjunior@ice.ufjf.br>
2022-01-06 19:10:47 -0300

    adiciona rota e pagina index

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ app.use(morgan("short"));
 app.use(serveStatic("./public"));
 app.use("/hello.html", helloWorld);
 app.use("/bye.html", byeWorld);
+app.use("/index.html", index);
 
 
 console.log("Servidor escutando em http://localhost:3000");
+
+function index(req, res, next){
+    res.setHeader("Content-Type", "text/html;charset=utf-8");
+    res.write("<link rel=stylesheet href=estilo.css />");
+    res.end(`
+    <h1>Olá, seja bem vindo a Atividade 4 da disciplina DCC195!</h1>
+    <ul>
+        <li><a href="./sobre.html">Sobre</a></li>
+        <li><a href="./techs.html">Techs</a></li>
+    </ul>            
+    `);
+}
 
 function helloWorld(req, res, next){
     res.setHeader("Content-Type", "text/html");
